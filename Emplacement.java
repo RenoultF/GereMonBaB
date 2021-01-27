d380ece38316191c84f1c28f6329042ed5c4750a
RenoultF <59865290+RenoultF@users.noreply.github.com>
2021-01-27 14:02:37 +0100

    Add files via upload

---
+++ Emplacement.java
@@ -0,0 +1,113 @@
+import java.util.*;
+
+
+public class Emplacement{
+
+    private int numEmplacement;
+    private int longueur;
+    private int largeur;
+
+    private String type;
+
+    private String statut;
+
+    private String proprietaire;
+
+    private int coordX;
+    private int coordY;
+
+    public Emplacement(int longueur, int largeur, String type, int x, int y, int numEmplacement){
+        this.longueur = longueur;
+        this.largeur = largeur;
+        this.statut = "LIBRE";
+        this.proprietaire = "NaN";
+        this.type = type;
+        this.coordX = x;
+        this.coordY = y;
+        this.numEmplacement = numEmplacement;
+    }
+
+    public void attribuerProPrietaire(String prop){
+        this.proprietaire = prop;
+    }
+
+    public void attribuerNumEmplacement(int num){
+        this.numEmplacement = num;
+    }
+
+    public void statutLibre(){
+        this.statut = "LIBRE";
+    }
+
+    public void statutEnCours(){
+        this.statut = "EN COURS";
+    }
+
+    public void statutReserve(){
+        this.statut = "RESERVE";
+    }
+
+    public boolean estReverse(){
+        return (this.statut == "RESERVE");
+    }
+
+    public boolean estLibre(){
+        return (this.statut == "LIBRE");
+    }
+
+    public boolean estEnCours(){
+        return (this.statut == "EN COURS");
+    }
+
+    public void modifierEmplacement(int nb){
+        this.numEmplacement += nb;
+    }
+
+    public boolean estStand(){
+        return (this.type == "STAND");
+    }
+
+    public boolean estDecoration(){
+        return (this.type != "STAND");
+    }
+
+    public int getNumEmplacement(){
+        return this.numEmplacement;
+    }
+
+    public int getLongueur(){
+        return this.longueur;
+    }
+
+    public int getLargeur(){
+        return this.largeur;
+    }
+
+    public String getStatut(){
+        return this.statut;
+    }
+
+    public String getProp(){
+        return this.proprietaire;
+    }
+
+    public String getType(){
+        return this.type;
+    }
+
+    public int getX(){
+        return coordX;
+    }
+
+    public int getY(){
+        return coordY;
+    }
+
+    public void afficheToi(){
+        System.out.println("Je suis de type :"+this.type+" mon num est " + this.numEmplacement + " et mon statut est  " + this.statut);
+    }
+
+    public Emplacement getEmpl(){
+        return this;
+    }
+}
---
+++ Emplacements.java
@@ -0,0 +1,86 @@
+import java.util.*;
+
+public class Emplacements{
+    LinkedList<Emplacement> stands ;
+    LinkedList<Emplacement> decorations;
+    private int nbStand;
+    private int nbDeco;
+
+    public Emplacements(){
+        stands = new LinkedList<>();
+        decorations = new LinkedList<> ();
+        nbStand = 0;
+        nbDeco = 0;
+    }
+
+
+    public Emplacement ajouterEmplacement(String type, int x, int y, int longueur, int largeur){
+        Emplacement empCourant = null;
+        if(type == "STAND"){
+            this.nbStand++;
+            empCourant = new Emplacement(longueur,largeur,type,x,y,this.nbStand);
+            stands.add(empCourant);
+        }
+        else{
+            this.nbDeco++;
+            empCourant = new Emplacement(longueur,largeur,type,x,y,this.nbDeco);       
+            decorations.add(empCourant);
+        }
+        return empCourant;
+    }
+
+    public void supprimerEmplacement(Emplacement emp){
+        if(emp.estStand()){
+            if(stands.size()>0){
+                for(int i = (stands.indexOf(emp));i<stands.size();i++){
+                   stands.get(i).modifierEmplacement(-1);
+                }
+                this.nbStand--;
+                stands.remove(emp);
+            }
+       }
+        else{
+            if(decorations.size()>0){
+                for(int i = (decorations.indexOf(emp));i<decorations.size();i++){
+                    decorations.get(i).modifierEmplacement(-1);
+                }
+                this.nbDeco--;
+                decorations.remove(emp);
+            }
+        }
+    }
+
+    public void toutAfficher(){
+        System.out.println("Pour les Stands :");
+        for(int i = 0;i < stands.size();i++){
+            stands.get(i).afficheToi();
+        }
+
+        System.out.println("Pour les Decorations :");
+        for(int i = 0;i < decorations.size();i++){
+            decorations.get(i).afficheToi();
+        }
+    }
+
+
+    public static void main(String[] args) {
+        Emplacements tabEmplacement = new Emplacements();
+        
+        Emplacement emp1 = tabEmplacement.ajouterEmplacement("STAND", 1, 1, 3, 2);
+        Emplacement emp2 = tabEmplacement.ajouterEmplacement("STAND", 1, 2, 3, 2);
+        Emplacement emp3 = tabEmplacement.ajouterEmplacement("STAND", 1, 3, 3, 2);
+        Emplacement emp4 = tabEmplacement.ajouterEmplacement("STAND", 1, 4, 3, 2);
+
+        Emplacement emp5 = tabEmplacement.ajouterEmplacement("BOULANGERIE", 2, 1, 3, 2);
+        Emplacement emp6 = tabEmplacement.ajouterEmplacement("BUVETTE", 2, 2, 3, 2);
+        Emplacement emp7 = tabEmplacement.ajouterEmplacement("POMPIER", 2, 3, 3, 2);
+        Emplacement emp8 = tabEmplacement.ajouterEmplacement("MAIRIE", 2, 4, 3, 2);
+        
+        tabEmplacement.toutAfficher();
+
+        tabEmplacement.supprimerEmplacement(emp2);
+        tabEmplacement.supprimerEmplacement(emp6);
+
+        tabEmplacement.toutAfficher();
+    }
+}
